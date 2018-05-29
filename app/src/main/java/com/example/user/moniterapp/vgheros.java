d75aa249777e90562283815beeec79bca7089de6
Aravindhan V <varavindhan2010@gmail.com>
2018-05-29 15:58:19 +0530

    Added list views to display the builds and counters

--- app/src/main/java/com/example/user/moniterapp/vgheros.java
+++ app/src/main/java/com/example/user/moniterapp/vgheros.java
@@ -2,17 +2,18 @@ package com.example.user.moniterapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.user.moniterapp.R;
+
+import java.util.ArrayList;
 
 public class vgheros extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vghero_layout);
-
-
+        setContentView(R.layout.vghero_layout); 
     }
 }
