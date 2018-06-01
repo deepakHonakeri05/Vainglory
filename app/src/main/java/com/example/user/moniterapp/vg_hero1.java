98f0d94908e5a32dfa4a45b01f4acad0fa574c5e
Aravindhan V <varavindhan2010@gmail.com>
2018-06-01 19:01:11 +0530

    Extra change

--- app/src/main/java/com/example/user/moniterapp/vg_hero1.java
+++ app/src/main/java/com/example/user/moniterapp/vg_hero1.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 
 public class vg_hero1 extends AppCompatActivity {
 
@@ -17,5 +18,16 @@ public class vg_hero1 extends AppCompatActivity {
         Intent intent = getIntent();
         int heroIndex = intent.getIntExtra("HeroName",-1);
         Log.i("Hero Selected is:",""+heroIndex);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return(super.onOptionsItemSelected(item));
     }
 }
