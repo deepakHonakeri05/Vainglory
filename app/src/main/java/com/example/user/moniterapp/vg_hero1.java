d7885edc2940ceee335fa867da850cdbb97fe49f
tenaciousDeepak <39375058+tenaciousDeepak@users.noreply.github.com>
2018-06-03 14:00:47 +0530

    added all counters(except kensei & malene)

--- app/src/main/java/com/example/user/moniterapp/vg_hero1.java
+++ app/src/main/java/com/example/user/moniterapp/vg_hero1.java
@@ -245,11 +245,159 @@ public class vg_hero1 extends AppCompatActivity {
         ar1.add(new Character(12,"Skye", R.drawable.skye));
 
 
+        ar1.add(new Character(13,"Churnwalker",R.drawable.churn));
+        ar1.add(new Character(13,"Lance", R.drawable.lance));
+        ar1.add(new Character(13,"Lyra", R.drawable.lyra));
+        ar1.add(new Character(13,"Petal", R.drawable.petal));
+        ar1.add(new Character(13,"Phinn", R.drawable.phinn));
+        ar1.add(new Character(13,"Skye", R.drawable.skye));
 
 
+        ar1.add(new Character(14,"Adagio",R.drawable.adagio));
+        ar1.add(new Character(14,"Alpha",R.drawable.alpha));
+        ar1.add(new Character(14,"Blackfeather", R.drawable.bf));
+        ar1.add(new Character(14,"Koshka", R.drawable.koshka));
+        ar1.add(new Character(14,"Krul", R.drawable.krul));
+        ar1.add(new Character(14,"Ringo",R.drawable.ringo));
+
+        ar1.add(new Character(15,"Blackfeather", R.drawable.bf));
+        ar1.add(new Character(15,"Krul", R.drawable.krul));
+        ar1.add(new Character(15,"Lyra", R.drawable.lyra));
+        ar1.add(new Character(15,"Rona",R.drawable.rona));
+        ar1.add(new Character(15,"Samuel", R.drawable.samuel));
+
+        ar1.add(new Character(16,"Baptiste", R.drawable.bap));
+        ar1.add(new Character(16,"Blackfeather", R.drawable.bf));
+        ar1.add(new Character(16,"Catherine", R.drawable.cat));
+        ar1.add(new Character(16,"Rona",R.drawable.rona));
+        ar1.add(new Character(16,"Skye", R.drawable.skye));
+
+        ar1.add(new Character(18,"Catherine", R.drawable.cat));
+        ar1.add(new Character(18,"Fortress", R.drawable.fortress));
+        ar1.add(new Character(18,"Glaive", R.drawable.glaive));
+        ar1.add(new Character(18,"Idris",R.drawable.idris));
+        ar1.add(new Character(18,"Petal", R.drawable.petal));
+        ar1.add(new Character(18,"Samuel", R.drawable.samuel));
+
+        ar1.add(new Character(19,"Baptiste", R.drawable.bap));
+        ar1.add(new Character(19,"Blackfeather", R.drawable.bf));
+        ar1.add(new Character(19,"Grumpjaw",R.drawable.grump));
+        ar1.add(new Character(19,"Lance", R.drawable.lance));
+        ar1.add(new Character(19,"Lyra", R.drawable.lyra));
+
+        ar1.add(new Character(20,"Lance", R.drawable.lance));
+        ar1.add(new Character(20,"Lorelai", R.drawable.lorelai));
+        ar1.add(new Character(20,"Petal", R.drawable.petal));
+        ar1.add(new Character(20,"Samuel", R.drawable.samuel));
+        ar1.add(new Character(20,"Skye", R.drawable.skye));
+
+        ar1.add(new Character(21,"Catherine", R.drawable.cat));
+        ar1.add(new Character(21,"Lyra", R.drawable.lyra));
+        ar1.add(new Character(21,"Phinn", R.drawable.phinn));
+        ar1.add(new Character(21,"Skye", R.drawable.skye));
+        ar1.add(new Character(21,"Vox",R.drawable.vox));
 
 
-        ar1.add(new Character(11,"-",R.drawable.lester));
+        ar1.add(new Character(22,"Catherine", R.drawable.cat));
+        ar1.add(new Character(22,"Celeste",R.drawable.celeste));
+        ar1.add(new Character(22,"Kestrel",R.drawable.kestrel));
+        ar1.add(new Character(22,"Koshka", R.drawable.koshka));
+        ar1.add(new Character(22,"Reza", R.drawable.reza));
+        ar1.add(new Character(22,"Taka", R.drawable.taka));
+
+        ar1.add(new Character(23,"Celeste",R.drawable.celeste));
+        ar1.add(new Character(23,"Samuel", R.drawable.samuel));
+        ar1.add(new Character(23,"Skaarf",R.drawable.skaarf));
+
+        ar1.add(new Character(25,"Ardan",R.drawable.ardan));
+        ar1.add(new Character(25,"Krul", R.drawable.krul));
+        ar1.add(new Character(25,"Lance", R.drawable.lance));
+        ar1.add(new Character(25,"Lyra", R.drawable.lyra));
+        ar1.add(new Character(25,"Reim", R.drawable.reim));
+        ar1.add(new Character(25,"Rona", R.drawable.rona));
+
+        ar1.add(new Character(26,"Celeste",R.drawable.celeste));
+        ar1.add(new Character(26,"Joule",R.drawable.joule));
+        ar1.add(new Character(26,"Lance", R.drawable.lance));
+        ar1.add(new Character(26,"Samuel", R.drawable.samuel));
+        ar1.add(new Character(26,"Skaarf",R.drawable.skaarf));
+        ar1.add(new Character(26,"Vox",R.drawable.vox));
+
+        ar1.add(new Character(28,"Kestrel",R.drawable.kestrel));
+        ar1.add(new Character(28,"Krul", R.drawable.krul));
+        ar1.add(new Character(28,"Lorelai", R.drawable.lorelai));
+        ar1.add(new Character(28,"Petal", R.drawable.petal));
+        ar1.add(new Character(28,"Skaarf",R.drawable.skaarf));
+        ar1.add(new Character(28,"Skye", R.drawable.skye));
+
+        ar1.add(new Character(29,"Alpha", R.drawable.alpha));
+        ar1.add(new Character(29,"Lyra", R.drawable.lyra));
+        ar1.add(new Character(29,"Phinn", R.drawable.phinn));
+        ar1.add(new Character(29,"Reim", R.drawable.reim));
+
+        ar1.add(new Character(30,"Catherine", R.drawable.cat));
+        ar1.add(new Character(30,"Flicker", R.drawable.flicker));
+        ar1.add(new Character(30,"Glaive", R.drawable.glaive));
+        ar1.add(new Character(30,"Kestrel",R.drawable.kestrel));
+        ar1.add(new Character(30,"Lance", R.drawable.lance));
+        ar1.add(new Character(30,"Taka", R.drawable.taka));
+
+        ar1.add(new Character(31,"Catherine", R.drawable.cat));
+        ar1.add(new Character(31,"Lorelai", R.drawable.lorelai));
+        ar1.add(new Character(31,"Lyra", R.drawable.lyra));
+        ar1.add(new Character(31,"Petal", R.drawable.petal));
+        ar1.add(new Character(31,"Skye", R.drawable.skye));
+        ar1.add(new Character(31,"Vox",R.drawable.vox));
+
+        ar1.add(new Character(32,"Ardan",R.drawable.ardan));
+        ar1.add(new Character(32,"Catherine", R.drawable.cat));
+        ar1.add(new Character(32,"Fortress", R.drawable.fortress));
+        ar1.add(new Character(32,"Glaive", R.drawable.glaive));
+        ar1.add(new Character(32,"Koshka", R.drawable.koshka));
+        ar1.add(new Character(32,"Taka", R.drawable.taka));
+
+        ar1.add(new Character(33,"Celeste",R.drawable.celeste));
+        ar1.add(new Character(33,"Glaive", R.drawable.glaive));
+        ar1.add(new Character(33,"Grace",R.drawable.grace));
+        ar1.add(new Character(33,"Joule",R.drawable.joule));
+        ar1.add(new Character(33,"Rona", R.drawable.rona));
+        ar1.add(new Character(33,"Skaarf",R.drawable.skaarf));
+        ar1.add(new Character(33,"Skye", R.drawable.skye));
+        ar1.add(new Character(33,"Vox",R.drawable.vox));
+
+        ar1.add(new Character(34,"Alpha", R.drawable.alpha));
+        ar1.add(new Character(34,"Glaive", R.drawable.glaive));
+        ar1.add(new Character(34,"Samuel", R.drawable.samuel));
+        ar1.add(new Character(34,"Taka", R.drawable.taka));
+        ar1.add(new Character(34,"Vox",R.drawable.vox));
+
+        ar1.add(new Character(35,"Ardan",R.drawable.ardan));
+        ar1.add(new Character(35,"Baptiste", R.drawable.bap));
+        ar1.add(new Character(35,"Petal", R.drawable.petal));
+        ar1.add(new Character(35,"Reza", R.drawable.reza));
+        ar1.add(new Character(35,"Taka", R.drawable.taka));
+
+        ar1.add(new Character(36,"Koshka", R.drawable.koshka));
+        ar1.add(new Character(36,"Lance", R.drawable.lance));
+        ar1.add(new Character(36,"Lyra", R.drawable.lyra));
+        ar1.add(new Character(36,"Petal", R.drawable.petal));
+
+        ar1.add(new Character(37,"Koshka", R.drawable.koshka));
+        ar1.add(new Character(37,"Krul", R.drawable.krul));
+        ar1.add(new Character(37,"Reza", R.drawable.reza));
+        ar1.add(new Character(37,"Taka", R.drawable.taka));
+
+        ar1.add(new Character(38,"Koshka", R.drawable.koshka));
+        ar1.add(new Character(38,"Krul", R.drawable.krul));
+        ar1.add(new Character(38,"Ozo", R.drawable.ozo));
+        ar1.add(new Character(38,"Reza", R.drawable.reza));
+        ar1.add(new Character(38,"Taka", R.drawable.taka));
+
+        ar1.add(new Character(39,"Adagio",R.drawable.adagio));
+        ar1.add(new Character(39,"Kestrel",R.drawable.kestrel));
+        ar1.add(new Character(39,"Koshka", R.drawable.koshka));
+        ar1.add(new Character(39,"Lorelai", R.drawable.lorelai));
+        ar1.add(new Character(39,"Ringo",R.drawable.ringo));
 
         for(int i=0;i<ar.size();i++){
             if(ar.get(i).getHeroIndex()==heroIndex){
