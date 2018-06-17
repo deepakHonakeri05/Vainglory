afaffa2a53b783b54dbdb3cf7f22e1f3587ca3d6
Aravindhan V <varavindhan2010@gmail.com>
2018-06-17 14:46:25 +0530

    Added Splash

---
+++ app/src/main/java/com/example/user/moniterapp/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.user.moniterapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIMEOUT = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },SPLASH_TIMEOUT);
+    }
+}
